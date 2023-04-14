5bb8a0b371f0366477daafc4abcd558c21c4aa2e
Xavier Fenton <xavierfenton99@gmail.com>
2023-04-14 20:15:01 +1200

    setup nav bar placeholders

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import '../styles/NavBar.css'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div className="nav-bar-wrapper">
       <div className="nav-bar-main">
         <ul>
-          <div>Home</div>
-          <div>World</div>
-          <div>Random</div>
-          <div>Contact</div>
+          <div>PAGES</div>
+          <div>
+            <Link to="/">1</Link>
+          </div>
+          <div>
+            <Link to="/">2</Link>
+          </div>
+          <div>
+            <Link to="/">3</Link>
+          </div>
+          <div>
+            <Link to="/">4</Link>
+          </div>
         </ul>
       </div>
     </div>
