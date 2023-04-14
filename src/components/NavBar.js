be3c88716175243ba45b9bb754a8978543f2bf62
Xavier Fenton <xavierfenton99@gmail.com>
2023-04-14 20:04:42 +1200

    create nav  comp

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="nav-bar-wrapper">
+      <div className="nav-bar-main">
+        <ul>
+          <div>Home</div>
+          <div>World</div>
+          <div>Random</div>
+          <div>Contact</div>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
