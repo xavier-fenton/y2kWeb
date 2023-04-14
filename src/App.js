a061467f07d81676d5c458f129273997470ff603
Xavier Fenton <xavierfenton99@gmail.com>
2023-04-14 20:04:26 +1200

    import nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 //create a basic architecture
+
+import NavBar from './components/NavBar'
+
 // import compenents
 function App() {
-  return <div className="App">HELLO</div>
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  )
 }
 
 export default App
