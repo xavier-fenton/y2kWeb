c26bb33d3b0c0db4661d26590d3e218b2300afbf
Xavier Fenton <xavierfenton99@gmail.com>
2023-04-15 01:19:39 +1200

    place comments for future

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,26 +11,40 @@ import '../styles/index.css'
   */
 
 const MainComponent = () => {
+  /* Trying to identify the state by the attribute 'open' in classes component-box-<number>
+    I could set the state of it being open and if open display it and if not display = none 
+  */
+  // var x = document.getElementsByClassName(`component-box-4`)[0]
+  // var open = x.hasAttribute('open')
+  // console.log(open)
+
+  // if (open === true) {
+  //   console.log(true)
+  // } else if (!open) {
+  //   console.log(false)
+  // }
+
+  console.log(x)
   return (
     <div className="Main-Component">
       <NavBar />
       <div className="main-body-container">
-        <div className="component-box-1">
+        <div className="component-box-1" open={true}>
           <div className="box-title">
             title <div>1</div>
           </div>
         </div>
-        <div className="component-box-2">
+        <div className="component-box-2" open={true}>
           <div className="box-title">
             title <div>2</div>
           </div>
         </div>
-        <div className="component-box-3">
+        <div className="component-box-3" open={true}>
           <div className="box-title">
             title <div>3</div>
           </div>
         </div>
-        <div className="component-box-4">
+        <div className="component-box-4" open={true}>
           <div className="box-title">
             title <div>4</div>
           </div>
