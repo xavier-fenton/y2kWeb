7713db43b4055ce394a68c9f55d34b9716808690
Xavier Fenton <xavierfenton99@gmail.com>
2023-04-17 21:40:41 +1200

    hard coded components refactor these in future

---
+++ src/components/subcomponents/Page1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../styles/TreePageBoxes/PageBoxes.css'
+
+const Page1 = () => {
+  return (
+    <div className="component-box-1" open={true}>
+      <div className="box-title">
+        title <div>1</div>
+      </div>
+    </div>
+  )
+}
+
+export default Page1
---
+++ src/components/subcomponents/Page2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../styles/TreePageBoxes/PageBoxes.css'
+
+const Page2 = () => {
+  return (
+    <div className="component-box-2" open={true}>
+      <div className="box-title">
+        title <div>2</div>
+      </div>
+    </div>
+  )
+}
+
+export default Page2
